1b4660df1b2804722f9e7a0c692ca5768b6c411a
marwamohamedasd <marwaamenasd@gmail.com>
2021-10-14 02:51:45 +0200

    Stackover

---
+++ resources/views/questions/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layout')
+
+
+
+@section('title')
+
+    questions
+@endsection
+
+
+
+@section('main')
+@include('session')
+<div class="P-3">
+<h3> All questions </h3>
+
+    @foreach($questions as $question)
+
+        <p> <a href="{{url("question/show/{$question->id}")}}"> {{$question->description}}    </a>  </p>
+        <p> created at:  {{$question->created_at}}</p>
+
+    @auth
+        <a href="{{route('question.edite',$question->id)}}" class="btn btn-success" role="button"> Edite </a>
+        <a href="{{route('delet.question',$question->id)}}" class="btn btn-danger" role="button">DELET</a>
+
+        @endauth
+
+        <hr>
+
+    @endforeach
+
+{{$questions->links()}}
+</div>
+@endsection
+
